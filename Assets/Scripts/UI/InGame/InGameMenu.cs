78f76ca26518b2563d029778ce2c6a6fb47d35b4
TiKebek <tokentondu@gmail.com>
2020-05-07 11:26:00 -0400

    Added RebindMenu to InGameMenu

--- Assets/Scripts/UI/InGame/InGameMenu.cs
+++ Assets/Scripts/UI/InGame/InGameMenu.cs
@@ -5,18 +5,28 @@ namespace Assets.Scripts.UI.InGame
     public class InGameMenu : MonoBehaviour
     {
         public GameSettingMenu GameSettingsMenu;
+        public RebindMenu RebindMenu;
+
+
         public void ShowGameSettingsMenu()
         {
             GameSettingsMenu.gameObject.SetActive(true);
         }
 
+        public void ShowRebindMenu()
+        {
+            RebindMenu.gameObject.SetActive(true);
+        }
+
         private void SetGameSettingsMenu()
         {
+
         }
 
         private void OnDisable()
         {
             GameSettingsMenu.gameObject.SetActive(false);
+            RebindMenu.gameObject.SetActive(false);
         }
 
         public void Quit()
